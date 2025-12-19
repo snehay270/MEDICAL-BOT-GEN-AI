8866cbb33666016c1afe9ae409c68d073f0c9759
snehay270 <snehay270@gmail.com>
2025-12-19 10:03:55 +0530

    changes done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Snehal Yadav",
+    author_email="snehay270@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
